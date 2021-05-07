cee5f7f05b6a7da671c284e34141dce989e4d16f
Patil-94 <ankitakhairnar10@.com>
2021-05-07 18:20:57 +0530

    [Ankita] Add:Welcome To Binary Search Tree

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("---------Welcome To Binary Search Tree--------");
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
