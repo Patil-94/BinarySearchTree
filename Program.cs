c037888f1e43af572d7dda9b1a25605c0e7d1ac0
Patil-94 <ankitakhairnar10@.com>
2021-05-09 18:27:20 +0530

    [Ankita] Add:Create Binary Search Tree

--- Program.cs
+++ Program.cs
@@ -13,6 +13,13 @@ namespace BinarySearchTree
 
             Console.WriteLine("---------Welcome To Binary Search Tree--------");
             Console.WriteLine();
+            BST<int> bST = new BST<int>(56);
+            bST.Insert(30);
+            bST.Insert(70);
+            bST.Display(); 
+                                          //    56
+                                       // 30         70
+        
             Console.Read();
         }
     }
