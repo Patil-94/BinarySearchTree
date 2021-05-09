a16649b379d76cb9b872c19e1b5f9e28927cdbad
Patil-94 <ankitakhairnar10@.com>
2021-05-09 19:28:53 +0530

    [Ankita] Add:Create Binary Search Node

--- Program.cs
+++ Program.cs
@@ -28,9 +28,11 @@ namespace BinarySearchTree
             bST.Insert(67);
            
             bST.GetSize();
-            bST.Display(); 
-                                        
-        
+            bST.Display();
+            bool result = bST.Search(63, bST);
+            Console.WriteLine();
+            Console.WriteLine("The element 63 exists in the BST: " + bST.Search(63, bST));
+
             Console.Read();
         }
     }
